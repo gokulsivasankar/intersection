274d706651846e5e23aa51c0f1bcfd6c4c2038ae
Gokul Sankar <goks@Gokuls-MacBook-Pro.local>
2019-02-07 11:57:43 -0500

    gokul

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import math
 
 import get_params
 import traff
+import numpy as np
 
 params = get_params.get_params()
 
@@ -26,6 +27,21 @@ for episode in range(1,params.max_episode+1):
     # traffic update
     traffic = traff.update(traffic,x_car,y_car,orientation_car,v_car,target_car)
 
-    Level_ratio = numpy.matrix([[0.1, 0.6, 0.3],[0.1, 0.6, 0.3]])
+    Level_ratio = np.matrix('0.1, 0.6, 0.3;0.1, 0.6, 0.3')
 
-    print(traffic.y)
+    for id in range(2,params.num_cars+1):
+        if id==2:
+            x_car = -0.5*params.w_lane
+            y_car = 4*params.w_lane
+            orientation_car = -math.pi/2
+            v_car = params.v_nominal
+            target_car = 1
+            
+    # traffic update
+    traffic = traff.update(traffic,x_car,y_car,orientation_car,v_car,target_car)
+
+    initial_state = np.block([[traffic.x],[traffic.y],[traffic.orientation],[traffic.v],[traffic.target]])
+
+    print(np.shape(initial_state))
+    print((initial_state[0,1]))
+        
